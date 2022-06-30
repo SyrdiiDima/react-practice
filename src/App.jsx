2302a7a66941d8460093cf892eeea587103632cc
SyrdiiDima <dimasyrdij@icloud.com>
2022-07-01 00:32:06 +0300

    2 task complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,26 @@
-import  Profile  from "./components/Profile/Profile";
+import Profile from "./components/Profile/Profile";
+import Statistics from './components/Statistics/Statistics'
 import user from './components/Profile/user.json'
+import data from './components/Statistics/data.json'
 import "./App.css";
 
 
 
 
 export const App = () => {
-  return <Profile
-  username={user.username}
-  tag={user.tag}
-  location={user.location}
-  avatar={user.avatar}
-    followers={user.stats.followers}
-    views={user.stats.views}
-    likes={user.stats.likes}
-/>
+  return (
+    <div className="container">
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        followers={user.stats.followers}
+        views={user.stats.views}
+        likes={user.stats.likes}
+      />
+
+      <Statistics title="Upload stats" stats={data} />
+      </div>
+  );
 }
---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,35 @@
+import PropTypes from 'prop-types'
+import css from './Statistics.module.css'
+
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+const Statistics = ({ title, stats }) => {
+    return  <section className={css.statistics}>
+    { title && <h2 className={css.title}>{title}</h2>}        
+        <ul className = {css.table}>
+             {stats.map(( {id, label, percentage}) => (
+             <li className={css.item} style = {{backgroundColor: getRandomHexColor()}} key={id}>
+                 <span className="label">{label}</span>
+                 <span className="percentage">{percentage}</span>
+             </li>
+            ))}
+        </ul>  
+    </section>
+}
+
+
+
+export default Statistics
+
+
+Statistics.propTypes = {
+  title: PropTypes.string.isRequired,
+  stats:PropTypes.arrayOf(PropTypes.shape).isRequired
+}
+// Statistics.propTypes = {
+//     title: PropTypes.string,
+//     stats:PropTypes.arrayOf(PropTypes.object).isRequired,   
+// }
